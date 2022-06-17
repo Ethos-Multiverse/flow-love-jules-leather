08dea95e747c08e6473e912756c227fdb3c6181f
Wielfried Zouantcha <wiel@zouantcha.com>
2022-06-17 10:41:04 -0400

    Unauthenticated state

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import * as fcl from "@onflow/fcl";
+import { useState } from "react";
+import "./flow/config";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [user, setUser] = useState();
+
+  // Authenticate user
+  const AuthedState = () => {
+    return (
+      <h1>Hey</h1>
+    )
+  }
+
+  const UnauthenticatedState = () => {
+    // setMessage("Connect Wallet");
+    return (
+      <div>
+        <button
+          className="bg-gray-100 hover:bg-gray-200 focus:bg-gray-200 border hover:border-gray-300 focus:border-gray-300 rounded shadow-lg absolute top-4 right-4 lg:top-8 lg:right-8 p-4 flex items-center text-xs disabled:cursor-not-allowed"
+          onClick={() => fcl.authenticate()}
         >
-          Learn React
-        </a>
-      </header>
+          {
+            <>
+              <span className="rounded-full h-2 w-2 block mr-2 bg-red-500" />
+            </>
+          }
+          {/* {message} */}
+          Connect Wallet
+        </button>
+        <div className="space-y-8">
+          <h1 className="text-4xl font-semibold mb-8">
+            Love Jules Leather Admin
+          </h1>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="max-w-xl mt-36 mx-auto px-4">
+      {user?.loggedIn ? <AuthedState /> : <UnauthenticatedState />}
     </div>
   );
 }
