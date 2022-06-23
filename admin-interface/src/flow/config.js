617596b246a0b3eb9c01dfcf1509911d24a89a49
Wielfried Zouantcha <wiel@zouantcha.com>
2022-06-23 14:31:03 -0400

    config client to flow testnet

---
+++ admin-interface/src/flow/config.js
@@ -0,0 +1,9 @@
+import { config } from "@onflow/fcl";
+
+// Use environment variables for easy deployment across environments
+config({
+  "accessNode.api": "https://rest-testnet.onflow.org",
+  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
+  "0xLoveJulesLeather": "0x3593499e2fbd34e3", 
+})
+
