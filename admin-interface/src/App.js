7eddcce2c3c134d527a0e51cd099ea5089f3e194
Wielfried Zouantcha <wiel@zouantcha.com>
2022-06-28 10:25:55 -0400

    import useState

--- admin-interface/src/App.js
+++ admin-interface/src/App.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState, useEffect } from 'react';
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import UserIcon from '@mui/icons-material/Group';
